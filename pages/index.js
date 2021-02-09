758570e192ad27ff16d9b4d4570f519d351bd3be
FelipeVash <felipe.vash.dev@gmail.com>
2021-02-09 12:53:48 -0300

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -6,5 +6,9 @@ const Title = styled.h1`
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return  (
+          <div>
+            <Title>Internet Caindo</Title>
+          </div>
+  )
 }
