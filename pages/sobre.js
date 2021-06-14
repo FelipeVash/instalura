ac48e6dcd6e309ea3c9ceb128f327eb3b224be56
Felipe Vash <felipe.silva@atonit.com.br>
2021-06-14 20:19:45 -0300

    feat: add graphQl

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import AboutScreen from '../src/components/screens/AboutScreen';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function sobre() {
-  return (
-    <div>
-      <p>Página Sobre</p>
-    </div>
-  );
+export async function getStaticProps() {
+  const TOKEN = '0aebf8f7bcffee44fbe032cf0e9cde';
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      pageSobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen);
